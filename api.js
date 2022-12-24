2a256c5a0f1ce260d932b32d76b5e3c81d8f9c65
jawhawk <mavenal2143@gmail.com>
2022-12-24 20:46:57 +0530

    async

--- api.js
+++ api.js
@@ -7,38 +7,38 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const url = "https://gogoanime.tel/";
-let animes = {};
 
 async function scraper(){
-try {
-  const { data } = await axios.get(url,{ 
-    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-});
-  const $ = cheerio.load(data);
+    let animes = {};
+    try {
+      const { data } = await axios.get( url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } });
+      const $ = cheerio.load(data);
+    
+      const items = $(".items li");
+    
+      items.each((ind,ele)=>{
+        const anime = {}
+        let k = $(ele).children()
+        let link = $(k['1']).children().first()
+        anime['name'] = $(k['1']).text();
+        anime['episode'] = parseInt(($(k['2']).text()).slice(8));
+        anime['link'] = url + $(link).attr('href');
+        animes[parseInt(ind)+1] = anime;
+      })
 
-  const items = $(".items li");
+      return animes
 
-  items.each((ind,ele)=>{
-    const anime = {}
-    let k = $(ele).children()
-    let link = $(k['1']).children().first()
-    anime['name'] = $(k['1']).text();
-    anime['episode'] = parseInt(($(k['2']).text()).slice(8));
-    anime['link'] = url + $(link).attr('href');
-    animes[parseInt(ind)+1] = anime;
-  })
-  
-} catch (error) {
-  console.log(error);
+    } catch (error) {
+      console.log(error);
+      return null
+    }
 }
-}
 
-scraper();
-setTimeout(()=>{scraper()},1000*60*5);
 //====================================================================================
 
-app.get('/', function(req, res) {
-    res.json(animes);
+app.get('/',async function(req, res) {
+    const data = await scraper();
+    res.json(data);
   });
 
 app.listen(process.env.PORT || 3000, function() {
